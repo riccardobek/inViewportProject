8c2aad47ed7fc090d96913b4ed5ca3c9dfbde08c
Riccardo <rikibek96@gmail.com>
2022-02-26 16:16:26 +0100

    Exemple of in Viewport project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgxInViewportModule } from '@ngx-lite/in-viewport';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxInViewportModule
   ],
   providers: [],
   bootstrap: [AppComponent]
